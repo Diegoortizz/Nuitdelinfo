d13bb1a816bb9d9cdd720cd0fb4343450ae7e843
Benjamin Bardy <benji_ba@hotmail.fr>
2018-12-06 22:44:10 +0100

    Base du jeu test

--- jeu.js
+++ jeu.js
@@ -1,62 +1,93 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.Canvas, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.image('atari', 'assets/sprites/atari130xe.png');
-	game.load.image('sky', 'assets/skies/sunset.png');
+    game.load.image('phaser', 'Graphics/bas.png');
+    game.load.spritesheet('veggies', 'Graphics/arbre.png', 48,32);
 
 }
 
 var sprite;
+var group;
 var cursors;
 
 function create() {
 
-    game.add.image(0, 0, 'sky');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	//	Enable p2 physics
-	game.physics.startSystem(Phaser.Physics.P2JS);
+    game.stage.backgroundColor = '#2d2d2d';
 
-    //  Make things a bit more bouncey
-    game.physics.p2.defaultRestitution = 0.8;
+    sprite = game.add.sprite(32, 200, 'phaser');
 
-    //  Add a sprite
-	sprite = game.add.sprite(200, 200, 'atari');
+    game.physics.arcade.enable(sprite);
+    
+    group = game.add.physicsGroup();
 
-    //  Enable if for physics. This creates a default rectangular body.
-	game.physics.p2.enable(sprite);
+    for (var i = 0; i < 50; i++)
+    {
+        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', 0);
+        c.name = "test";
+        console.log(c.name);
+    }
 
-    //  Modify a few body properties
-	sprite.body.setZeroDamping();
-	sprite.body.fixedRotation = true;
-
-    text = game.add.text(20, 20, 'move with arrow keys', { fill: '#ffffff' });
+    for (var i = 0; i < 20; i++)
+    {
+        var c = group.create(game.rnd.between(100, 770), game.rnd.between(0, 570), 'veggies', 1);
+        c.body.immovable = true;
+    }
 
     cursors = game.input.keyboard.createCursorKeys();
 
+    game.time.events.add(Phaser.Timer.SECOND * 10, end, this);
+}
+
+function end(){
+    console.log("stop");
 }
 
 function update() {
 
-	sprite.body.setZeroVelocity();
+    if (game.physics.arcade.collide(sprite, group, collisionHandler, processHandler, this))
+    {
+        console.log('boom');
+    }
+
+    // game.physics.arcade.overlap(sprite, group, collisionHandler, null, this);
+
+    sprite.body.velocity.x = 0;
+    sprite.body.velocity.y = 0;
 
     if (cursors.left.isDown)
     {
-    	sprite.body.moveLeft(400);
+        sprite.body.velocity.x = -200;
     }
     else if (cursors.right.isDown)
     {
-    	sprite.body.moveRight(400);
+        sprite.body.velocity.x = 200;
     }
 
     if (cursors.up.isDown)
     {
-    	sprite.body.moveUp(400);
+        sprite.body.velocity.y = -200;
     }
     else if (cursors.down.isDown)
     {
-    	sprite.body.moveDown(400);
+        sprite.body.velocity.y = 200;
     }
+}
+
+function processHandler (player, veg) {
+
+    return true;
+
+}
+
+function collisionHandler (player, veg) {
+    veg.truc = 5;
+    console.log(veg.animations.currentFrame.index);
+    console.log(veg);
+    veg.kill();
+
 
 }
